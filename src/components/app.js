260926dcacf399c98baf699ff63c170e41ce7e97
Alireza <alireza97hi@gmail.com>
2017-08-03 12:52:57 +0430

    simple reminder added

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <div>
+          <label htmlFor="reminder-request">Type your Reminder</label>
+          <input className="form-control" type="text" name="reminder-request" />
+        </div>
+        <button className="btn btn-success">Add Reminder</button>
+      </div>
     );
   }
 }
